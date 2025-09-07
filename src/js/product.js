669fed3b2eba2e8e436e0deadf3dfd7939b784d5
Eric Garcia <133874193+ericgarcia11@users.noreply.github.com>
2025-09-06 21:45:20 -0300

    add to card task done

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cardItems = getLocalStorage("so-cart") || [];
+  cardItems.push(product);
+  setLocalStorage("so-cart", cardItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
