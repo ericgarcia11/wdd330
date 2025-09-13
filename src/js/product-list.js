3988b50015611dacd84beb6c041cbc54b06e7f5a
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-09-13 13:28:50 -0300

    done brooo

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -6,20 +6,20 @@ fetch("json/tents.json")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
-        data.forEach(product => {
-            const clone = template.content.cloneNode(true);
-            const [a, img, h3, h2, p] = clone.querySelectorAll("a, img, h3, h2, p");
 
-            a.href = `product_pages/product_page.html?productId=${product.Id}`;
-            img.src = product.Image;
-            img.alt = product.Brand.Name;
-            h3.textContent = product.Brand.Name;
-            h2.textContent = product.NameWithoutBrand;
-            p.textContent = `$${product.FinalPrice}`;
+        if(data.Id != "880RR" && data.Id != "989CG"){
+            data.forEach(product => {
+                const clone = template.content.cloneNode(true);
+                const [a, img, h3, h2, p] = clone.querySelectorAll("a, img, h3, h2, p");
 
-            contaier.appendChild(clone);
-        });
+                a.href = `product_pages/product_page.html?productId=${product.Id}`;
+                img.src = product.Image;
+                img.alt = product.Brand.Name;
+                h3.textContent = product.Brand.Name;
+                h2.textContent = product.NameWithoutBrand;
+                p.textContent = `$${product.FinalPrice}`;
+
+                contaier.appendChild(clone);
+            });
+        }    
     });
-
-
