25d633f868afdc9776e7d61e364b934d7feb6c98
Kevin <kferreira@byupathway.edu>
2025-09-23 13:52:08 -0300

    :o

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -2,17 +2,16 @@
 const template = document.getElementById("product-list-template");
 const contaier = document.getElementById("product-list-container");
 
-function renderListWithTemplate(){
-    fetch("/json/tents.json")
+fetch("/json/tents.json")
     .then(response => response.json())
     .then(data => {
-        // console.log(data);
+        console.log(data);
         
         data.forEach(product => {
             const clone = template.content.cloneNode(true);
             const [a, img, h3, h2, p] = clone.querySelectorAll("a, img, h3, h2, p");
 
-            a.href = `product_pages/index.html?product=${product.Id}`;
+            a.href = `product_pages/product_page.html?productId=${product.Id}`;
             img.src = product.Image;
             img.alt = product.Brand.Name;
             h3.textContent = product.Brand.Name;
@@ -22,6 +21,3 @@ function renderListWithTemplate(){
             contaier.appendChild(clone);
         });    
     });
-};
-
-renderListWithTemplate();
